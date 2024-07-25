6f1d837e6944add161e335f5141e6efb89b228eb
navadenis <nvadns06e24d416i@iisbadoni.edu.it>
2024-07-25 15:19:55 +0200

    Updating home page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home',)]
+    public function loadHomePage(): Response
+    {
+        return $this->render('homePage.html');
+    }
+
+    #
+}
+?>
