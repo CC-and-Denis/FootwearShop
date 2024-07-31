d50625b4350a74c4c9cd7daff59ae5edc391e34a
colozzacristian <Cristian.Colozza@protonmail.com>
2024-07-31 16:25:53 +0200

    userPage almost done

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,9 +4,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/home',)]
     public function loadHomePage(): Response
     {
@@ -17,6 +26,24 @@ class HomeController extends AbstractController
     public function loadProductPage(): Response
     {
         return $this->render('productpage.html.twig');
+    }
+
+    #[Route('user/{username}',)]
+    public function loadUserPage(String $username): Response
+    {
+        $userRepository = $this->entityManager->getRepository(\App\Entity\User::class);
+        $targetUser = $userRepository->findOneBy(['username' => $username]);
+
+        if($targetUser){
+            return $this->render('userPage.html.twig',[
+                'username'=>$username,
+                'isVendor'=>$targetUser->isVendor(),
+            ]);
+        }else{
+            return $this->render('notFound.html.twig',[
+                'entity'=>"User"
+            ]);
+        }
     }
 }
 ?>
